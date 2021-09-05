4e2e7ea7737d29b05b59f1cc9345a14b9c0261eb
Ubuntu <ubuntu@ip-172-31-28-125.eu-west-1.compute.internal>
2021-09-05 21:59:20 +0000

    databaselayerrelationships

--- create.py
+++ create.py
@@ -1,3 +1,9 @@
-from app import db
+from app import db, Aeroplanes, Flights
 
 db.create_all()
+
+boeing_747= Aeroplanes(model_number = 5674538, number_of_seats = 102, company_owned_by ="Emirates")
+db.session.add(boeing_747)
+db.session.commit()
+
+
