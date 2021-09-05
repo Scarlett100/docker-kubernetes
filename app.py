47cd4a8b06543c4c78ed9eb369e1f9c09b991d5f
Ubuntu <ubuntu@ip-172-31-28-125.eu-west-1.compute.internal>
2021-09-05 20:21:01 +0000

    making requirements, databaselayerconfig

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 @app.route('/home')
@@ -9,7 +14,8 @@ def home():
 
 @app.route('/about')
 def about():
-    return 'This is the about page'
+   return 'On this website you can make a flight scheduele for a flight you would like to take'
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
