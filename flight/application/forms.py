e156abe0c12ade05b8d64eba4101f97d445e4a88
Scarlett100 <m.scarlettstewart@gmail.com>
2021-09-07 22:37:55 +0000

    forms

--- flight/application/forms.py
+++ flight/application/forms.py
@@ -6,20 +6,20 @@ from application.models import Aeroplanes
 from application.models import Flights
 
 class AeroplanesForm(FlaskForm):
-    model_number = SelectField("model", choices=[("747"),
-    ("812"),
-    ("989")
+    model_number = SelectField("model", choices=[(747, 747),
+    (812, 812),
+    (989, 989)
     ])
     number_of_seats = SelectField("number of seats", choices=[
-        ("40"),
-        ("90"), 
-        ("173")
+        (40, 40),
+        (90, 90), 
+        (173, 173)
     ])
     company_owned_by = SelectField("owner", choices=[
-        ("Emirates"), 
-        ("British Airways"), 
-        ("Virgin Atlantic"), 
-        ("Air France")
+        ("Emirates", "Emirates"), 
+        ("British Airways", "British Airways"), 
+        ("Virgin Atlantic", "Virgin Atlantic"), 
+        ("Air France", "Air France")
     ])
     
 
