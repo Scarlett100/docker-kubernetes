8cd04e1890378e2ed552073bc0eaf12348bdeed1
Scarlett100 <m.scarlettstewart@gmail.com>
2021-09-07 22:38:31 +0000

    models

--- flight/application/models.py
+++ flight/application/models.py
@@ -2,22 +2,27 @@ from application import db
 
 
 class Aeroplanes(db.Model):
+     
     aeroplane_id = db.Column(db.Integer, primary_key=True) 
     model_number = db.Column(db.Integer, nullable=False)
     number_of_seats = db.Column(db.Integer, nullable=False)
     company_owned_by = db.Column(db.String(30), nullable=False)
     flights = db.relationship('Flights', backref='aeroplane')
 
-    #def __repr__(self):
+    def __repr__(self):
+        return '<Aeroplanes%r>' % self.id
         #return
     
 
 class Flights(db.Model):
     flight_id = db.Column(db.Integer, primary_key=True) 
     departure_date_time = db.Column(db.DateTime, nullable=False)
-    arrival_date_time = db.Column(db.DateTime, nullable=False)
+    sarrival_date_time = db.Column(db.DateTime, nullable=False)
     arrival_destination = db.Column(db.String(20), nullable=False)
     direct_flight = db.Column(db.Boolean, default=True)
     flight_price = db.Column(db.Float, nullable=False)
     fk_aeroplane_id = db.Column(db.Integer, db.ForeignKey('aeroplanes.aeroplane_id'), nullable=False) 
    
+    def __repr__(self):
+        return '<Flights%r>' % self.id
+        
